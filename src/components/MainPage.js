31a49a3bb26b09310cf13a025ff9eec2791a1410
이영훈 <1024201@naver.com>
2022-07-09 01:26:31 +0900

    7월9일

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,40 +1,56 @@
 import React from 'react';
 import DehazeIcon from '@mui/icons-material/Dehaze';
+import styled, { keyframes } from 'styled-components';
 import Box from './Box';
 import Chart from './Chart';
 import SideBar from './SideBar';
-import { keyframes } from 'styled-components';
 
-let toggle = true;
-const sideBar = () => {
-  toggle = !toggle;
-  if (toggle) {
-    document.getElementById('sideBarId').style.display = 'block';
-  } else {
-    document.getElementById('sideBarId').style.display = 'none';
-  }
-};
-const animation = keyframes`
+const sideBarIn(){
+  const animation = keyframes`
  from {margin-left:-300px;}
  to{margin-left:0;}
 
 `;
 
-const SideBar = styled.div`
+styled(SideBar)`
   animation: ${animation} 1s linear infinite;
 `;
+}
+const sideBarOut(){
+const animation = keyframes`
+ 
+ from{margin-left:0;}
+ t0 {margin-left:-300px;}
+
+`;
+
+styled(SideBar)`
+  animation: ${animation} 1s linear infinite;
+`;
+}
+
+let toggle = true;
+const sideBar = () => {
+  toggle = !toggle;
+  if (toggle) {
+    sideBarIn();
+  } else {
+    sideBarOut();
+  }
+};
+
 
 sideBar();
 const MainPage = () => {
   return (
     <div>
       <DehazeIcon onClick={sideBar} />
-      <SideBar id="sideBarId" />
+      <SideBar />
       <div style={{ display: 'flex' }}>
         <Box description="현재 칼로리 섭취량" calories="40" />
         <Box description="허용된 칼로리 섭취 잔량" calories="360" />
       </div>
-      <Chart />
+      //주석<Chart />
     </div>
   );
 };
