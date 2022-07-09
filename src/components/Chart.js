71c9d8115833ac1f5c35ea952a4ede636593b1af
이영훈 <1024201@naver.com>
2022-07-09 12:53:18 +0900

    refactor: 안쓰는 코드 제거

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { Bar } from "react-chartjs-2";
-import styled from "styled-components";
-
+// import styled from "@emotion/styled";
+/*
+const Container = styled.div`
+  width: 90vw;
+  max-width: 900px;
+`;
+*/
 const Chart = () => {
-  const Container = styled.div`
-    width: 90vw;
-    max-width: 900px;
-  `;
-
   return <Container>{/* <Bar type="bar" data={data} /> */}</Container>;
 };
 
