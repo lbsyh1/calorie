8f377fb31dcaf089079068c891ab241c4c0493ae
이영훈 <1024201@naver.com>
2022-07-16 12:20:02 +0900

    chore: 불필요한 icon제거, Wrapper, PageWrapper 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "@emotion/styled";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "@/page/Home";
@@ -6,24 +7,36 @@ import Search from "@/page/Search";
 import AddFoodCalorie from "@/page/AddFoodCalorie";
 import MyCalorieList from "@/page/MyCalorieList";
 import EditFoodCalorie from "@/page/EditFoodCalorie";
-import DehazeIcon from "@mui/icons-material/Dehaze";
+
 import SideBar from "./components/SideBar";
+
+const Wrapper = styled.div`
+  width: 100%;
+  max-width: 500px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+const PageWrapper = styled.section`
+  margin-top: 50px;
+`;
 function App() {
   return (
-    <div className="App">
-      <DehazeIcon />
-
+    <Wrapper className="App">
       <BrowserRouter>
         <SideBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="search" element={<Search />} />
-          <Route path="my-calories" element={<MyCalorieList />} />
-          <Route path="my-calories/add" element={<AddFoodCalorie />} />
-          <Route path="my-calories/edit" element={<EditFoodCalorie />} />
-        </Routes>
+        <PageWrapper>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="search" element={<Search />} />
+            <Route path="my-calories" element={<MyCalorieList />} />
+            <Route path="my-calories/add" element={<AddFoodCalorie />} />
+            <Route path="my-calories/edit" element={<EditFoodCalorie />} />
+          </Routes>
+        </PageWrapper>
       </BrowserRouter>
-    </div>
+    </Wrapper>
   );
 }
 
