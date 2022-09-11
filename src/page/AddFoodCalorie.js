284193edcd11b7c268492363217922bd56826ddc
이영훈 <1024201@naver.com>
2022-09-12 08:47:47 +0900

    feat(service): add food calorie

--- src/page/AddFoodCalorie.js
+++ src/page/AddFoodCalorie.js
@@ -1,35 +1,84 @@
-import React from "react";
-import SearchBarOfSearch from "../components/SearchBar";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import ControlPointIcon from "@mui/icons-material/ControlPoint";
 import FoodRecord from "../components/FoodRecord";
+import { createCalorie } from "@/service/CalorieService";
+const Wrapper = styled.div`
+  border: 1px solid black;
+  background: #ffe9cf;
+`;
+const Section = styled.div`
+  width: 80%;
+  height: 80%;
+`;
+const DisplayFlex = styled.div`
+  display: flex;
+`;
+const TextBox = styled.section`
+  position: relative;
+  margin: 20px 0;
 
+  .add-icon {
+    position: absolute;
+    right: 20px;
+    width: 30px;
+    height: 30px;
+  }
+
+  p + p {
+    margin-top: 10px;
+  }
+`;
 export default function () {
-  const Wrapper = styled.div`
-    border: 1px solid black;
-    background: #ffe9cf;
-  `;
-  const Section = styled.div`
-    width: 80%;
-    height: 80%;
-  `;
-  const DisplayFlex = styled.div`
-    display: flex;
-  `;
+  const dateRef = useRef(null);
+  const foodRef = useRef(null);
+  const takeRef = useRef(null);
+  const calorieRef = useRef(null);
 
   return (
     <div>
       <p>AddFoodCalorie</p>
-      <p>
-        음식먹은 날짜 : <input type="date"></input>
-      </p>
+      <TextBox>
+        <ControlPointIcon
+          className="add-icon"
+          onClick={() => {
+            if (
+              !dateRef.current.value ||
+              !foodRef.current.value ||
+              !takeRef.current.value ||
+              !calorieRef.current.value
+            ) {
+              alert("데이터를 입력해주세요");
+              return;
+            }
 
-      <DisplayFlex>
-        <Section>
-          <SearchBarOfSearch />
-        </Section>
-        <ControlPointIcon />
-      </DisplayFlex>
+            createCalorie({
+              date: dateRef.current.value,
+              totalCalories: calorieRef.current.value,
+              numberOfIntakes: takeRef.current.value,
+              foodName: foodRef.current.value,
+            });
+          }}
+        />
+
+        <p>
+          <label>식사 날짜: </label>
+          <input id="date" type="date" ref={dateRef}></input>
+        </p>
+
+        <p>
+          <label>음식 이름: </label>
+          <input type="text" ref={foodRef}></input>
+        </p>
+        <p>
+          <label>인분: </label>
+          <input type="number" defaultValue={1} ref={takeRef}></input>
+        </p>
+        <p>
+          <label>칼로리: </label>
+          <input type="number" ref={calorieRef}></input> kcal
+        </p>
+      </TextBox>
 
       <Wrapper>
         <FoodRecord foodName="피자" amount="1/4판" calories="250kcal" />
--- src/service/CalorieService.js
+++ src/service/CalorieService.js
@@ -1,6 +1,19 @@
+import { v4 as uuidv4 } from "uuid";
+
 // CRUD (Create, Read, Update, Delete)
 export const getCalories = async () => {
   const response = await fetch("http://localhost:8000/calories");
   const data = await response.json();
   return data;
+};
+
+export const createCalorie = async (data) => {
+  const response = await fetch("http://localhost:8000/calories", {
+    method: "POST",
+    body: JSON.stringify({ id: uuidv4(), ...data }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+  return response;
 };
